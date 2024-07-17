df0d5f4408836ef3832cabd827486f733dea0119
Ahmed Salman <117323971+AhmedSalman1@users.noreply.github.com>
2024-07-17 14:02:55 +0300

    Time: 75 ms (85.30%) | Memory: 16.9 MB (80.01%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -4,8 +4,8 @@ class Solution:
 
         while l <= r:
             if s[l] != s[r]:
-                st1 = s[:l] + s[l+1:]
-                st2 = s[:r] + s[r+1:]
+                st1 = s[l+1:r+1]
+                st2 = s[l:r]
                 return st1 == st1[::-1] or st2 == st2[::-1]
             l += 1
             r -= 1
