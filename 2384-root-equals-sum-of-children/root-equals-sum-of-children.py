1cb9b98c324ecec61d94d01155e5f2a2b0b26be1
Ahmed Salman <117323971+AhmedSalman1@users.noreply.github.com>
2024-09-08 08:08:23 +0300

    Time: 37 ms (44.50%) | Memory: 16.3 MB (85.52%) - LeetSync

--- 2384-root-equals-sum-of-children/root-equals-sum-of-children.py
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.py
@@ -6,6 +6,4 @@
 #         self.right = right
 class Solution:
     def checkTree(self, root: Optional[TreeNode]) -> bool:
-        if root.val == (root.left.val + root.right.val):
-            return True
-        return False
+        return root.val == root.left.val + root.right.val
