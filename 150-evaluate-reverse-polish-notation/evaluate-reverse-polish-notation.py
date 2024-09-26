568dabcfab2357d23e35062b45ba2c84a63f019c
Ahmed Salman <117323971+AhmedSalman1@users.noreply.github.com>
2024-09-26 15:30:07 +0300

    Time: 66 ms (53.48%) | Memory: 17.1 MB (61.97%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -15,30 +15,3 @@ class Solution:
             else:
                 stack.append(int(c))
         return stack[0]
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # stack = []
-        # for c in tokens:
-        #     if c == '+':
-        #         stack.append(stack.pop() + stack.pop())
-        #     elif c == '-':
-        #         a, b = stack.pop(), stack.pop()
-        #         stack.append(b - a)
-        #     elif c == '*':
-        #         stack.append(stack.pop() * stack.pop())
-        #     elif c == '/':
-        #         a, b = stack.pop(), stack.pop()
-        #         stack.append(b // a)
-        #     else:
-        #         stack.append(int(c))
-        # return stack[0]
