4fdc8b21cbfd958e1ea391978f910120601a8910
Ahmed Salman <a7medsalman6@gmail.com>
2025-02-09 20:20:50 +0200

    Time: 0 ms (100.00%) | Memory: 18 MB (22.95%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -6,7 +6,7 @@ class Solution:
         last = m + n - 1
 
         while m > 0 and n > 0:
-            if nums1[m - 1] >= nums2[n - 1]:
+            if nums1[m - 1] > nums2[n - 1]:
                 nums1[last] = nums1[m - 1]
                 m -= 1
             else:
